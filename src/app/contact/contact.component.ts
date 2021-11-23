f46596ab59ba2f0e8117b654615b9a38f9c6aa22
Fhjorth <43441370+Fhjorth@users.noreply.github.com>
2021-11-23 13:10:37 +0100

    scrollspy + firebase

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, Validators, FormControl, NgForm} from '@angular/forms';
+import {FormBuilder, FormGroup, Validators, FormControl, NgForm, Form} from '@angular/forms';
 import {ContactService} from '../contact.service';
+import {HttpClient} from '@angular/common/http';
+import validate = WebAssembly.validate;
 
 @Component({
   selector: 'app-contact',
@@ -9,12 +11,26 @@ import {ContactService} from '../contact.service';
 })
 export class ContactComponent implements OnInit {
 
-  FormData: FormGroup;
+  form: FormGroup;
+  name: FormControl = new FormControl('', [Validators.required]);
+  email: FormControl = new FormControl('', [Validators.required, Validators.email]);
+  message: FormControl = new FormControl('', [Validators.required, Validators.maxLength(256)]);
+  honeypot: FormControl = new FormControl('');
+  submitted: boolean = false;
+  isLoading: boolean = false;
+  responsMessage: string;
 
-  constructor(private builder: FormBuilder, private contact: ContactService) { }
+  constructor(private formBuilder: FormBuilder, private contact: ContactService, private http: HttpClient) {
+    this.form = this.formBuilder.group({
+      name: this.name,
+      email: this.email,
+      message: this.message,
+      honeypot: this.honeypot
+    });
+  }
 
   ngOnInit(): void {
-    this.FormData = this.builder.group({
+    this.form = this.formBuilder.group({
       Fullname: new FormControl('', [Validators.required]),
       Email: new FormControl('', [Validators.compose([Validators.required,
       Validators.email])]),
@@ -22,15 +38,34 @@ export class ContactComponent implements OnInit {
     });
   }
 
-  onSubmit(FormDate) {
-    console.log(FormDate);
-    this.contact.PostMessage(FormDate)
-      .subscribe(response => {
-        location.href = '';
-        console.log(response);
-      }, error => {
-        console.warn(error.responseText);
-        console.log({error});
-      });
+  onSubmit() {
+    if (this.form.status == 'VALID') {
+      this.form.disable();
+      var formData: any = new FormData();
+      formData.append("name", this.form.get("name").value);
+      formData.append("email", this.form.get("email").value);
+      this.isLoading = true;
+      this.submitted = false;
+      this.http.post("fgjkldjfglk", formData).subscribe(
+        (response) => {
+          if (response["result"] == "success") {
+            this.responsMessage = "Thanks for the message! I'll get back to you soon!";
+          } else {
+            this.responsMessage = "Opps! something went wrong.. reload the page and try again.";
+          }
+          this.form.enable();
+          this.submitted = true;
+          this.isLoading = false;
+          console.log(response);
+        },
+        (error => {
+          this.responsMessage = "Opps! An error occurred.. Reload the page and try again.";
+          this.form.enable();
+          this.submitted = true;
+          this.isLoading = false;
+          console.log(error);
+        })
+      );
+    }
   }
 }
